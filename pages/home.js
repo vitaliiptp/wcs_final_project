f46ff18cc4669b988c2b0ccd3e715cc0bb53aef8
vitaliiptp <vitalii.potapenko@gmail.com>
2022-01-18 15:37:04 +0100

    init Next JS project + Tailwind

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center bg-emerald-200 font-medium text-6xl text-red-700">
+            Page for testing Tailwind
+        </div>
+    );
+};
+
+export default Home;
