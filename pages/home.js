4fc05fea8616ba2fa88a1af29171e514b81a6589
vitaliiptp <vitalii.potapenko@gmail.com>
2022-01-19 12:21:06 +0100

    addefolder with starter code to desigh system

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{dispay: "flex"}}>
             <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-gradient-blue via-gradient-purple to-gradient-pink font-Jost font-medium text-6xl">
                 Page for testing Tailwind
             </div>
