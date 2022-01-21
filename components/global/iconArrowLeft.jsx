fdd77ff27af57f9a705512a3ed585de36ad30a70
Anna Jaksics <anna.jaksics@gmail.com>
2022-01-21 12:30:23 +0100

    new-feedback-page

---
+++ components/global/iconArrowLeft.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const iconArrowLeft = () => (
+    <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L2 5l4-4" stroke="#4661E6" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
+);
+
+export default iconArrowLeft;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,31 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'Jost': ['Jost', ...defaultTheme.fontFamily.sans],
+        'primary': ['Jost', ...defaultTheme.fontFamily.sans],
       },
     },
     colors: {
+      'body-background': '#F2F2F2',
       'blue-normal': '#4661E6',
-'blue-light': '#62BCFA',
-'blue-dark': '#373F68FF',
-'blue-deep': '#3A4374',
-'blue-gardient': '#28A7ED',
-'grey-light': '#647196',
-'pink-gardient': '#E84D70',
-'purple-normal': '#AD1FEA',
-'purple-gardient': '#A337F6',
-'salmon-normal': '#F49F85',
-'white-normal': '#FFFFFF',
-'white-light': '#F7F8FD',
-'white-dark': '#F2F4FF'
-}
+      'blue-normal-hover': '#7C91F9',
+      'blue-light': '#62BCFA',
+      'blue-dark': '#373F68',
+      'blue-deep': '#3A4374',
+      'blue-deep-hover': '#656EA3',
+      'blue-gardient': '#28A7ED',
+      'grey-light': '#647196',
+      'pink-gardient': '#E84D70',
+      'purple-normal': '#AD1FEA',
+      'purple-normal-hover': '#C75AF6',
+      'purple-gardient': '#A337F6',
+      'red-normal': '#D73737',
+      'red-normal-hover': '#E98888',
+      'salmon-normal': '#F49F85',
+      'white-normal': '#FFFFFF',
+      'white-light': '#F7F8FD',
+      'white-dark': '#F2F4FF',
+      'white-dark-hover': '#CFD7FF',
+    }
   },
   plugins: [],
 }
