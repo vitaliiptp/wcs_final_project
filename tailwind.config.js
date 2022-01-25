bbe6546ee04441e7c99b28d31662c7a6161f3cbb
vitaliiptp <vitalii.potapenko@gmail.com>
2022-01-25 13:46:10 +0100

    fixed tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,7 @@ module.exports = {
       'purple-normal': '#AD1FEA',
       'purple-normal-hover': '#C75AF6',
       'purple-gradient': '#A337F6',
+      'purple-light': '#8C92B3',
       'red-normal': '#D73737',
       'red-normal-hover': '#E98888',
       'salmon-normal': '#F49F85',
@@ -33,8 +34,6 @@ module.exports = {
       'white-light': '#F7F8FD',
       'white-dark': '#F2F4FF',
       'white-dark-hover': '#CFD7FF',
-    }
-      'body-background': '#F2F2F2'
     },
   },
   plugins: [],
