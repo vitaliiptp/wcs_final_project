79cab131b57103c33a34f946e26632a38e6ebb5c
vitaliiptp <vitalii.potapenko@gmail.com>
2022-01-25 09:56:39 +0100

    added comments to feedba page

--- components/comments.jsx
+++ components/comments.jsx
@@ -5,10 +5,13 @@ import CommentCard from "./global/commentCard";
 const Comments = ({ feedbackId }) => {
     const { items } = useContext(AppContext);
 
+    const commentsArray = items.filter((item) => item.id === parseInt(feedbackId)).map((item) => item.comments);
+
     return (
         <div className="rounded-[0.625rem] bg-white-normal">
-            {items.filter((item) => item.id === parseInt(feedbackId)).map((item) => item.comments).map((comment, index) =>
-                <CommentCard key={`comment${index}`} image="" name={comment[index].user.name} username={comment[index].user.username} content={comment[index].content}/>
+            {commentsArray.map((comment, index) =>
+                console.log(comment)
+                // <CommentCard key={`comment${index}`} image="" name={comment[index].user.name} username={comment[index].user.username} content={comment[index].content}/>
             )}
         </div>
     );
