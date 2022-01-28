dd075f9d3d46072006e5d90e79b46b6baabdf967
Anna Jaksics <anna.jaksics@gmail.com>
2022-01-28 12:29:29 +0100

    added-dropdow-textfield

--- pages/newfeedback.js
+++ pages/newfeedback.js
@@ -1,6 +1,14 @@
 import React from "react";
 import IconNewFeedback from "../components/global/iconNewFeedback";
 import IconArrowLeft from "../components/global/iconArrowLeft";
+import Dropdown from "../components/global/Dropdown";
+import Textfield from "../components/global/Textfield";
+
+const dropdownOptions = ['Feature', 'UI', 'UX', 'Enhancement', 'Bug'];
+// Feedback Title 
+// Add a short, descriptive headline 
+
+
 
 const newFeedback = () => {
     return (
@@ -18,40 +26,8 @@ const newFeedback = () => {
                 <div className="w-[28.5rem] mb-10 mt-[3.25rem]">
                     <span className="text-b-24">Create New Feedback</span>
                 </div>   
-                <div className="flex flex-col items-start mb-6">
-                    <label className="text-b-14" htmlFor="feedbackTitle">Feedback Title</label>
-                    <span className="text-n-14 mb-4">Add a short, descriptive headline</span>
-                    <input id="feedbackTitle" type="text" maxLength={46}
-                        className=" 
-                        m-0 
-                        w-[28.5rem]
-                        min-h-[3rem] 
-                        px-6 
-                        border-white-light 
-                        rounded-[0.313rem] 
-                        focus:border-blue-normal 
-                        invalid:border-red-normal 
-                        bg-white-light 
-                        text-blue-deep" 
-                    />
-                </div>
-                <div className="flex flex-col items-start mb-6">
-                    <label className="text-b-14" htmlFor="feedbackCategory">Category</label>
-                    <span className="text-n-14 mb-4">Choose a category for your feedback</span>
-                    <input id="feedbackCategory" type="text" maxLength={46}
-                        className=" 
-                        m-0 
-                        w-[28.5rem]
-                        min-h-[3rem] 
-                        px-6 
-                        border-white-light 
-                        rounded-[0.313rem] 
-                        focus:border-blue-normal 
-                        invalid:border-red-normal 
-                        bg-white-light 
-                        text-blue-deep" 
-                    />
-                </div>
+                <Textfield label='Feedback Title' description='Add a short, descriptive headline'/>
+                <Dropdown label='Category' description='Choose a category for your feedback' options={dropdownOptions}/>
                 <div className="flex flex-col items-start">
                     <label className="text-b-14" htmlFor="feedbackCategory">Feedback Detail</label>
                     <span className="text-n-14 mb-4">Include any specific comments on what should be improved, added, etc.</span>
@@ -60,7 +36,8 @@ const newFeedback = () => {
                         m-0 
                         w-[28.5rem]
                         min-h-[6rem] 
-                        px-6 
+                        px-6
+                        border-[0.063rem]
                         border-white-light 
                         rounded-[0.313rem] 
                         focus:border-blue-normal 
@@ -71,8 +48,8 @@ const newFeedback = () => {
                 </div>
                 <div className="w-[28.5rem] h-full flex justify-end items-center">
                     <div className="">
-                        <button className="mr-4 rounded-[0.625rem] px-6 pb-[0.719rem] pt-[0.781rem] bg-blue-deep hover:bg-blue-deep-hover text-b-14-w">Cancel</button>
-                        <button className="rounded-[0.625rem] px-6 pb-[0.719rem] pt-[0.781rem] bg-purple-normal hover:bg-purple-normal-hover text-b-14-w">Add Feedback</button>
+                        <button className="text-white-dark mr-4 rounded-[0.625rem] px-6 pb-[0.719rem] pt-[0.781rem] bg-blue-deep hover:bg-blue-deep-hover text-b-14-w">Cancel</button>
+                        <button className="text-white-dark rounded-[0.625rem] px-6 pb-[0.719rem] pt-[0.781rem] bg-purple-normal hover:bg-purple-normal-hover text-b-14-w">Add Feedback</button>
                     </div>
                 </div>
             </div>
