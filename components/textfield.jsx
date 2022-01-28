3fc2a1d78e32334c055b33f004b694a41b419285
Anna Jaksics <anna.jaksics@gmail.com>
2022-01-28 14:15:59 +0100

    added-dropdow-textfield-to-new-feedback-page

---
+++ components/textfield.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+export default function Textfield({label, description}) {
+
+  return (
+    <div className="flex flex-col items-start mb-6">
+        <label className="text-b-14" htmlFor="feedbackCategory">{label}</label>
+        <span className="text-n-14 mb-4">{description}</span>
+        <input 
+            id="feedbackTitle" 
+            type="text" 
+            maxLength={50}
+            className="m-0 w-[28.5rem] min-h-[3rem] px-6 rounded-[0.313rem] border-[0.063rem] border-white-light hover:border-blue-normal focus:border-blue-normal  bg-white-light text-blue-deep text-left"/>
+    </div>
+  );
+}
